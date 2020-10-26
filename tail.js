1065718fcd2d44102eb25e7b79227e4cbe7116d9
Stephan <Stephanpauld@gmail.com>
2020-10-26 22:31:36 +0000

    assertion tail

---
+++ tail.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴️ Assertion Failed: ${actual} !== ${expected}`);
+
+  }
+};
+
+const tail = function(arr) {
+  return arr.slice(1);
+};
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
