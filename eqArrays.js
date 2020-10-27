7d2ded3946f54dd6b34400d1505cb81e0b8d8b9f
Stephan <Stephanpauld@gmail.com>
2020-10-27 21:03:20 +0000

    Checking equal arrays lotide proj

--- eqArrays.js
+++ eqArrays.js
@@ -10,6 +10,11 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(arr1, arr2)  {
   let index = 0;
+
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
   for (let digit in arr1) {
 
     if (arr1[digit] === arr2[index]) {
@@ -24,4 +29,4 @@ const eqArrays = function(arr1, arr2)  {
 
 
 
-assertEqual(eqArrays([1, 2, '3'], [1, 2, '3']), true);
+assertEqual(eqArrays([1, 2, '3', 11], [1, 2, '3']), true);
