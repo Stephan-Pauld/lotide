564f85eca6ea06659e43dd9a234d053b3cc51039
Stephan <Stephanpauld@gmail.com>
2020-10-29 21:19:14 +0000

    checking for keys LOTIDE

---
+++ findKey.js
@@ -0,0 +1,48 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅️ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴️ Assertion Failed: ${actual} !== ${expected}`);
+
+  }
+};
+
+const findKey = function(obj, callback) {
+  for (const item in obj) {
+    if (callback(obj[item])) {
+      return item;
+    }
+  }
+};
+
+let result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)
+
+let result2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3)
+
+let result3 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 1)
+
+
+assertEqual(result1, 'noma')
+assertEqual(result2, "Akaleri")
+assertEqual(result3, 'Akelarre')
