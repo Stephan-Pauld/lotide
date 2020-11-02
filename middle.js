e205b987d61eba3386c5b8af19936da850155f71
Stephan <stephanpauld@gmail.com>
2020-11-02 14:31:31 -0700

    Refactoring Lotide learing import/export

--- middle.js
+++ middle.js
@@ -1,44 +1,21 @@
-const eqArrays = function(arr1, arr2)  {
-  let index = 0;
 
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let digit in arr1) {
-
-    if (arr1[digit] === arr2[index]) {
-      index ++;
-
-    } else {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅️ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴️ Assertion Failed: ${actual} !== ${expected}`);
-
-  }
-};
 
 const middle = function(array) {
   let length = array.length - 1;
   let middle = Math.round(length / 2);
   let newArr = [];
-  if (!(length % 2 === 0)) {
-    newArr.push(array[middle]);
+  if (array.length <= 2) {
+    return [];
+
+  } else if (!(length % 2 === 0)) {
     newArr.push(array[middle -1]);
+    newArr.push(array[middle]);
     return newArr;
+
   } else {
     return array[middle];
   }
 };
 
 
-
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]));
+module.exports = middle;
---
+++ test/middle.test.js
@@ -0,0 +1,11 @@
+const middle = require('../middle');
+const assertArrayEqual = require('../assertArrayEqual');
+
+
+
+
+
+assertArrayEqual(middle([1, 2, 3, 4]), [2, 3])
+assertArrayEqual(middle([1, 2, 3, 4, 5]), 3)
+assertArrayEqual(middle([1, 2]), [])
+assertArrayEqual(middle([1]), [])
