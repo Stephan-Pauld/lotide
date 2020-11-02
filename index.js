81a37d382d1aa8c326e7eb6af9445b3ac987afeb
Stephan <stephanpauld@gmail.com>
2020-11-02 15:42:04 -0700

    using index.js to hold our object of functions

---
+++ index.js
@@ -0,0 +1,13 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const assertEqual = require('./assertEqual')
+const assertArrayEqual = require('./assertArrayEqual')
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  assertEqual,
+  assertArrayEqual
+};
