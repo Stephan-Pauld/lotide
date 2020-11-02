adb1f5707ee45dcda496da91333f7f278247abc6
Stephan <stephanpauld@gmail.com>
2020-11-02 13:27:51 -0700

    Refactoring Lotide learning import/export

--- eqArrays.js
+++ eqArrays.js
@@ -1,20 +1,11 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅️ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴️ Assertion Failed: ${actual} !== ${expected}`);
-
-  }
-};
-
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arr1, arr2)  {
   let index = 0;
-
+  
   if (arr1.length !== arr2.length) {
     return false;
   }
-
   for (let digit in arr1) {
 
     if (arr1[digit] === arr2[index]) {
@@ -29,4 +20,4 @@ const eqArrays = function(arr1, arr2)  {
 
 
 
-assertEqual(eqArrays([1, 2, '3', 11], [1, 2, '3']), true);
+module.exports = eqArrays;
